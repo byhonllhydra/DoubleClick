14ec9313795e87a55e5c7d5813ee749d5285082d
Braydon Hall <bjrhall@outlook.com>
2017-06-15 23:09:01 +1000

    Changed MainWindow to DoubleClickWindow

---
+++ DoubleClick/DoubleClick.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DoubleClick
+{
+  /// <summary>
+  /// Interaction logic for DoubleClickWindow.xaml
+  /// </summary>
+  public partial class DoubleClickWindow : Window
+  {
+    public DoubleClickWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
